3cb432f72ada1f939304b49c2f2452798850925e
joeloffbeat <joeloffbeat@gmail.com>
2026-02-24 10:00:00 +0530

    feat(webapp): implement home page

--- packages/webapp/src/app/page.tsx
+++ packages/webapp/src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Surely</h1>
-      <p>Parametric insurance protocol</p>
-    </main>
-  );
+  redirect("/policies");
 }
